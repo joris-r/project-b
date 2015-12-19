ffe7a850323e41bef73238dded1a87a6ce7dc41f
Joris Rehm <joris.rehm@wakusei.fr>
2015-12-19 23:11:48 +0100

    scan mono-line comments

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,20 @@ enum Token {
 fn test_trivial(){
     assert_eq!(scan(" "), vec![Token::Spaces(0,1)]);
     assert_eq!(scan(" \n\t"), vec![Token::Spaces(0,3)]);
-    // assert_eq!(scan("//x"), vec![Token::Comment(0,3)]);
-    // assert_eq!(scan("// one"), vec![Token::Comment(0,6)]);
+    assert_eq!(scan("//x"), vec![Token::Comment(0,3)]);
+    assert_eq!(scan("// one"), vec![Token::Comment(0,6)]);
+}
+
+#[test]
+fn test_double(){
+    assert_eq!(scan("//xy z\n  "), vec![
+        Token::Comment(0,6),
+        Token::Spaces(6,9),
+    ]);
+    assert_eq!(scan("\n  //xy z"), vec![
+        Token::Spaces(0,3),
+        Token::Comment(3,9),
+    ]);
 }
 
 struct ScannerState {
@@ -22,37 +34,55 @@ struct ScannerState {
 
 fn scan(source : & str) -> Vec<Token> {
     let mut state = ScannerState{ i:0, j:0, token : Token::Error };
-    scan_spaces(source, &mut state);
-    // scan_comment(source, &mut state);
-    vec![state.token]
+    let mut res = vec![];
+    loop {
+        scan_spaces(source, &mut state);
+        scan_comment_monoline(source, &mut state);
+        if state.token == Token::Error {
+            return res
+        }
+        res.push(state.token);
+        state.i = state.j;
+        state.token = Token::Error;
+    }
 }
 
 fn scan_spaces(source : &str, state : &mut ScannerState){
-    let mut l = state.i;
+    let mut x = state.i;
     loop {
-        match source.chars().nth(l) {
-            Some(' ') | Some('\t') | Some('\n') => l += 1,
+        match source.chars().nth(x) {
+            Some(' ') | Some('\t') | Some('\n') => x += 1,
             _ => break,
         }
     }
-    if state.j < l {
-        state.j = l;
-        state.token = Token::Spaces(state.i, l)
+    if state.j < x {
+        state.j = x;
+        state.token = Token::Spaces(state.i, x)
     }
 }
 
-// fn scan_comment(source : &str, state : &mut ScannerState){
-    // let mut l = state.i;
-
-// }
-
-fn main() {
-    println!("{:?}", scan(" \n\t"));
+fn scan_comment_monoline(source : &str, state : &mut ScannerState){
+    let mut x = state.i;
+    if source.chars().nth(x) == Some('/') &&
+       source.chars().nth(x+1) == Some('/') {
+        x += 2;
+        loop {
+            match source.chars().nth(x) {
+                None => break,
+                Some('\n') => break,
+                Some(_) => x += 1,
+            }
+        }
+    }
+    if state.j < x {
+        state.j = x;
+        state.token = Token::Comment(state.i, x)
+    }
 }
 
-
-
-
+fn main() {
+    println!("{:?}", scan("//xy z\n//a "));
+}
 
 
 
