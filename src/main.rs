23a382b62ec122ebe971a1319bce6f2db54f34b1
Joris Rehm <joris.rehm@wakusei.fr>
2016-01-03 11:25:49 +0100

    a small test about ordered enumeration

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,57 @@ mod scanner;
 use syntax::Token;
 
 fn main() {
+    let mut size = 0;
+    let mut list = vec!["".to_string()];
+    loop {
+        match give_one(&mut size, &mut list) {
+            Some(s) => println!("{:?}",s),
+            None => break
+        }
+    }
+}
+
+fn give_one(size : &mut usize, list : &mut Vec<String>) -> Option<String> {
+    loop {
+        match list.pop() {
+            None => {
+                *size += 1;
+                list.push("".to_string());
+            },
+            Some(s) => {
+                if s.len() == *size {
+                    return Some(s);
+                } else if s.len() > *size {
+                    continue;
+                } else {
+                    list.push(s.clone() + "a");
+                    list.push(s.clone() + "bbb");
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+fn enum_rec(){
+    for n in 0..7 {
+        enum_rec_f(n, "".to_string());
+    }
+}
+
+fn enum_rec_f(n : usize, s : String){
+    if n == s.len() {
+        println!("{}",s);
+        return;
+    }
+    if n < s.len() {
+        return;
+    }
+    enum_rec_f(n, s.clone() + "a");
+    enum_rec_f(n, s.clone() + "bb");
+}
+
+fn test() {
     
     let mut i = 0;
     for (src,expected) in genlex::new().take(121) {
