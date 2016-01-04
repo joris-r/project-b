158126acf3892f7c290bd8619cba68b28034693b
Joris Rehm <joris.rehm@wakusei.fr>
2016-01-04 21:52:11 +0100

    redo token generation with a new way

--- src/genlex.rs
+++ src/genlex.rs
@@ -1,83 +1,98 @@
 
 use syntax::Token;
 
+const SPACES_CYCLE : [char; 3] = ['\n', '\t', ' '];
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum GenToken {
-    Spaces(char),
+    Spaces(String),
 }
 
 pub struct LexGen {
-    next : Vec<GenToken>,
-}
-
-const SPACES_CYCLE : [char; 3] = [' ', '\t', '\n'];
-
-impl LexGen {
-
-    fn increment(&mut self) {
-        let mut i = 0;
-        loop {
-            if i >= self.next.len() {
-                self.next.push(GenToken::Spaces(SPACES_CYCLE[0]));
-                break;
-            } else {
-                let x = match self.next[i] {
-                    GenToken::Spaces(c) =>
-                        SPACES_CYCLE.iter().position(|x| *x==c).unwrap()
-                    };
-                if x + 1 < SPACES_CYCLE.len() {
-                    self.next[i] = GenToken::Spaces(SPACES_CYCLE[x+1]);
-                    break;
-                } else {
-                    self.next[i] = GenToken::Spaces(SPACES_CYCLE[0]);
-                    i += 1;
-                }
-            }
-        }
-    }
-    
-    fn generate(&self) -> (String, Vec<Token>) {
-
-        let mut res_str = String::new();
-        let mut res_tok = vec![];
-        let mut before = 0;
-        
-        for t in self.next.clone() {
-            match t {
-                GenToken::Spaces(s) => {
-                    res_str.push(s);
-                    match res_tok.last() {
-                        Some(& Token::Spaces(a,b)) =>  {
-                            res_tok.pop();
-                            res_tok.push(Token::Spaces(a,b+1));
-                            // TODO possible to modify in place with a mutable pattern ?
-                        }
-                        _ => {
-                            res_tok.push( Token::Spaces(before, res_str.len()) );
-                        }
-                    }
-                    before = res_str.len();
-                }
-            }
-        }
-        
-        (res_str, res_tok)
-    }
-    
+    list : Vec<Vec<GenToken>>,
+    size : usize,
 }
 
 pub fn new() -> LexGen {
     LexGen{
-        next : vec![],
+        list : vec![vec![]],
+        size : 0,
     }
+}
+
+fn length_list(src : &Vec<GenToken>) -> usize{
+    src.iter().fold(0, |sum, ref val| {sum + length_token(val)})
+}
+
+fn length_token(token : &GenToken) -> usize{
+    match token {
+        &GenToken::Spaces(ref s) => s.len()
+    }
+}
+
+impl LexGen {
+
+    fn enumerate(&mut self) -> Vec<GenToken> {
+        loop {
+            match self.list.pop() {
+                None => {
+                    self.size += 1;
+                    self.list.push(vec![]);
+                },
+                Some(elem) => {
+                    if length_list(&elem) == self.size {
+                        return elem;
+                    } else if length_list(&elem) > self.size {
+                        continue;
+                    } else {
+                        for c in SPACES_CYCLE.iter() {
+                            let mut new = elem.clone();
+                            new.push(GenToken::Spaces(c.to_string()));
+                            self.list.push(new);
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    
+}
+    
+fn generate(res : &Vec<GenToken>) -> (String, Vec<Token>) {
+
+    let mut res_str = String::new();
+    let mut res_tok = vec![];
+    let mut before = 0;
+    
+    for t in res.clone() {
+        match t {
+            GenToken::Spaces(s) => {
+                res_str.push_str(&s);
+                match res_tok.last() {
+                    Some(& Token::Spaces(a,b)) =>  {
+                        res_tok.pop();
+                        res_tok.push(Token::Spaces(a,b+s.len()));
+                        // TODO possible to modify in place with a mutable pattern ?
+                    }
+                    _ => {
+                        res_tok.push( Token::Spaces(before, res_str.len()) );
+                    }
+                }
+                before = res_str.len();
+            }
+        }
+    }
+    
+    (res_str, res_tok)
 }
 
 impl Iterator for LexGen {
     type Item = (String,Vec<Token>);
     
     fn next(&mut self) -> Option<(String,Vec<Token>)> {
-        let res = Some(self.generate());
-        self.increment();
+        let res = self.enumerate();
+        let res = Some(generate(&res));
         res
     }
 }
@@ -102,20 +117,21 @@ fn test_gen() {
         "\n".to_string(), vec![
             Token::Spaces(0,1),
         ])));
+        
     assert_eq!(gen.next(), Some((
         "  ".to_string(), vec![
             Token::Spaces(0,2),
         ])));
     assert_eq!(gen.next(), Some((
-        "\t ".to_string(), vec![
-            Token::Spaces(0,2),
-        ])));
-    assert_eq!(gen.next(), Some((
-        "\n ".to_string(), vec![
-            Token::Spaces(0,2),
-        ])));
-    assert_eq!(gen.next(), Some((
         " \t".to_string(), vec![
+            Token::Spaces(0,2),
+        ])));
+    assert_eq!(gen.next(), Some((
+        " \n".to_string(), vec![
+            Token::Spaces(0,2),
+        ])));
+    assert_eq!(gen.next(), Some((
+        "\t ".to_string(), vec![
             Token::Spaces(0,2),
         ])));
     assert_eq!(gen.next(), Some((
