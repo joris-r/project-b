bc1237baa2be01c190d2165dc6b1c4b5a403541c
Joris Rehm <joris.rehm@wakusei.fr>
2016-01-08 22:35:58 +0100

    improve style: add an impl ScannerState

--- src/scanner.rs
+++ src/scanner.rs
@@ -21,18 +21,26 @@ fn test_double(){
     ]);
 }
 
-struct ScannerState {
+struct ScannerState<'a> {
     i : usize,
     j : usize,
     token : Token,
+    source : &'a str,
 }
 
-pub fn scan(source : & str) -> Vec<Token> {
-    let mut state = ScannerState{ i:0, j:0, token : Token::Error };
+pub fn scan(source : &str) -> Vec<Token> {
+    let mut state = ScannerState{
+        i:0,
+        j:0,
+        token : Token::Error,
+        source : source,
+    };
     let mut res = vec![];
     loop {
-        scan_spaces(source, &mut state);
-        scan_comment_monoline(source, &mut state);
+    
+        state.scan_spaces();
+        state.scan_comment_monoline();
+        
         if state.token == Token::Error {
             return res
         }
@@ -42,59 +50,48 @@ pub fn scan(source : & str) -> Vec<Token> {
     }
 }
 
-fn scan_spaces(source : &str, state : &mut ScannerState){
-    let mut x = state.i;
-    loop {
-        match source.chars().nth(x) {
-            Some(' ') | Some('\t') | Some('\n') => x += 1,
-            _ => break,
-        }
-    }
-    if state.j < x {
-        state.j = x;
-        state.token = Token::Spaces(state.i, x)
-    }
-}
+impl<'a> ScannerState<'a> {
 
-fn scan_comment_monoline(source : &str, state : &mut ScannerState){
-    let mut x = state.i;
-    if source.chars().nth(x) == Some('/') &&
-       source.chars().nth(x+1) == Some('/') {
-        x += 2;
+    fn scan_spaces(&mut self){
+        let mut x = self.i;
         loop {
-            match source.chars().nth(x) {
-                None => break,
-                Some('\n') => break,
-                Some(_) => x += 1,
+            match self.source.chars().nth(x) {
+                Some(' ') | Some('\t') | Some('\n') => x += 1,
+                _ => break,
             }
         }
+        if self.j < x {
+            self.j = x;
+            self.token = Token::Spaces(self.i, x)
+        }
     }
-    if state.j < x {
-        state.j = x;
-        state.token = Token::Comment(state.i, x)
+
+    fn scan_comment_monoline(&mut self){
+        let mut x = self.i;
+        if self.source.chars().nth(x) == Some('/') &&
+           self.source.chars().nth(x+1) == Some('/') {
+            x += 2;
+            loop {
+                match self.source.chars().nth(x) {
+                    None => break,
+                    Some('\n') => break,
+                    Some(_) => x += 1,
+                }
+            }
+        }
+        if self.j < x {
+            self.j = x;
+            self.token = Token::Comment(self.i, x)
+        }
     }
+    
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// TODO scan_comment_multiline
+// TODO scan_number
+// TODO scan_identifier
+// TODO scan_keyword
+// TODO scan_operator
 
 
