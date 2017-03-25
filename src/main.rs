a0d288b1ac4d415c58ad8a46a78db02a1a9d7045
Joris Rehm <joris.rehm@wakusei.fr>
2017-03-25 22:46:59 +0100

    remove custom scanner

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-extern crate unicode_normalization;
 
-mod syntax;
-mod scanner;
 mod parser_lalrpop;
 mod mbast;
 
@@ -9,8 +6,7 @@ mod mbast;
 
 fn main() {
     let src = "IMPLEMENTATION m0_i REFINES m0 CONSTRAINTS 2+2=4 END";
-    let tok = scanner::scan(src);
-    let res = parser_lalrpop::parse_Component(src, tok);
+    let res = parser_lalrpop::parse_Component(src);
     println!("{:?}", res);
 }
 
