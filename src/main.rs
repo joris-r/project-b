ed190d1ef501ca0556964ec46b0eeb0bc5013513
Joris Rehm <joris.rehm@wakusei.fr>
2017-03-26 22:53:35 +0200

    progress on parser: add some substitutions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod mbast;
 
 fn main() {
     let src = "IMPLEMENTATION m0_i REFINES m0 IMPORTS toto(2+2)
-    INITIALISATION BEGIN xx := 10 END END";
+    INITIALISATION CHOICE xx := 10 OR yy := 200 END
+    END";
     let res = parser_lalrpop::parse_Component(src);
     println!("{:?}", res);
 }
