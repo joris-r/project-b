fe270218e4edc8666fdad52f134963dafc0d2663
Joris Rehm <joris.rehm@wakusei.fr>
2017-03-26 21:59:30 +0200

    progress on the parser

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod mbast;
 
 
 fn main() {
-    let src = "IMPLEMENTATION m0_i REFINES m0 CONSTRAINTS 2+2=4 END";
+    let src = "IMPLEMENTATION m0_i REFINES m0 IMPORTS toto(2+2)
+    INITIALISATION BEGIN xx := 10 END END";
     let res = parser_lalrpop::parse_Component(src);
     println!("{:?}", res);
 }
